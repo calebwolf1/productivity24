b0d439db3b02f3c1680ebf4dabca9e243a67f999
Caleb Wolf <calebwolf42@gmail.com>
2024-03-13 19:13:05 -0500

    firebase setup + basic pages

---
+++ src/firebase-config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore"
+const firebaseConfig = {
+  apiKey: "AIzaSyD04TRCBDkLwRenckydsV3yagzHJZiFUeA",
+  authDomain: "productivity24-9396f.firebaseapp.com",
+  projectId: "productivity24-9396f",
+  storageBucket: "productivity24-9396f.appspot.com",
+  messagingSenderId: "537394445602",
+  appId: "1:537394445602:web:800f735d3d98b38605b97b",
+  measurementId: "G-G6YZ2XD8YT"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
---
+++ src/pages/Home.js
@@ -0,0 +1,8 @@
+
+const Home = () => {
+  return (
+    <header>Hello home page!</header>
+  )
+}
+
+export default Home;
