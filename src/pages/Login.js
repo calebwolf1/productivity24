9978a6192940969656fb22403af9f5fcb2b6f020
jasmineball511 <jasball123@gmail.com>
2024-04-16 16:36:30 -0500

    corner gradient on login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,13 @@
 import './Login.css';
 import '../Fonts.css';
 import Auth from "../components/Auth"
+import Logo from '../whoosh.png';
 
 const Login = () => {
   return (
     <div>
+      <div className="box"></div>
+      <img src={Logo} alt="Logo" className='logo'/>;
       <div className='welcome'>Send confidently, Whoosh</div>
       <div className='login'>Login</div>
       <Auth />
